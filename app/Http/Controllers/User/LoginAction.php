1e6c313be0678685997231b7d43ed7cacc22c6de
isanasan <yfcgpsebp@gmail.com>
2021-06-18 18:51:08 +0900

    add LoginAction Controller

---
+++ app/Http/Controllers/User/LoginAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Responder\TokenResponder;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTGuard;
+
+class LoginAction extends Controller
+{
+    private $authManager;
+
+    public function __construct(AuthManager $authManager)
+    {
+        $this->authManager = $authManager;
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, TokenResponder $responder): JsonResponse
+    {
+        /** @var JWTGuard $guard */
+        $guard = $this->authManager->guard('jwt');
+        $token = $guard->attempt([
+            'email' => $request->get('email'),
+            'password' => $request->get('password'),
+        ]);
+
+        return $responder(
+            $token,
+            $guard->factory()->getTTL() * 60
+        );
+    }
+}
